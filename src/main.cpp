ff7f619e7e3b0faf48158e22936a778b50cf3610
Davide Sordi <23182484+sordinho@users.noreply.github.com>
2019-03-01 14:29:13 +0100

    first commit
    dht11 working, using lcd

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 #include "DHT.h"
 #include "Adafruit_Sensor.h"
+#include "LiquidCrystal_I2C.h"
+#include "Wire.h"
 
 #define DHTPIN 2 // Arduino pin connected to the sensor
 #define DHTTYPE DHT11 //We're using DHT11 sensor
+
+LiquidCrystal_I2C lcd(0x3F,16,2); //set LCD address
 
 //Sensor inizialization
 DHT dht(DHTPIN,DHTTYPE);
@@ -11,6 +15,13 @@ DHT dht(DHTPIN,DHTTYPE);
 void setup() {
   Serial.begin(9600);
   Serial.println("Temperature and Humidity sensor test.");
+
+  lcd.init(); // initialize the lcd 
+  lcd.backlight();
+
+  lcd.print("Temp [C]: ");
+  lcd.setCursor(0,1);
+  lcd.print("Umidit [%]: ");
 
   dht.begin();
 }
@@ -22,16 +33,14 @@ void loop() {
   delay(5000); //Wait 5 seconds between 2 measurements
 
   temp = dht.readTemperature(); //read temperature (Celsius as default)
-  hum = dht.readHumidity();
+  hum = dht.readHumidity(); //read humidity
 
   if(isnan(temp)||isnan(hum)){
     Serial.println("ERROR: FAILED TO READ FROM SENSOR!!!");
     return;
   }
-  Serial.println("--------------------------------------");
-  Serial.print("Temperature: ");
-  Serial.println(temp);
-  Serial.print("Humidity: ");
-  Serial.println(hum);
-  Serial.println("--------------------------------------");
+  lcd.setCursor(10,0);
+  lcd.print(temp,1);
+  lcd.setCursor(12,1);
+  lcd.print(hum,0);
 }
